abd470e335f391556046f3618e572d644510c929
xtave <xtave@yandex-team.ru>
2023-02-01 02:40:39 +0300

    1

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
+import axios from "axios";
+
+const authorize = ({
+  default_avatar_id: defaultAvatarId,
+  display_name: displayName,
+}) => {
+  const avatarHtml = `<div class="avatar" style="background-image:url('https://avatars.mds.yandex.net/get-yapic/${defaultAvatarId}/islands-middle')"></div>`;
+  const nameHtml = `<div class="name">${displayName}</div>`;
+
+  document.getElementById("auth").innerHTML = `${avatarHtml}${nameHtml}`;
+};
+
+const fetchYandexData = (token) =>
+  axios.get(`https://login.yandex.ru/info?format=json&oauth_token=${token}`);
+
 window.onload = () => {
-  console.log("Hello world!");
+  // TODO
 };
