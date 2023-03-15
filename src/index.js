787babd86e77988b66f07958d7c7c6264bb05db2
trofimovauls <127831060+trofimovauls@users.noreply.github.com>
2023-03-15 08:23:28 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -21,12 +21,12 @@ window.onload = () => {
       {
         client_id: "d8294a45168b4ecbbe5265119d88d2d7",
         response_type: "token",
-          redirect_uri: "https://oauth-master-class-iota.vercel.app/token.html",
-        },
-      https://oauth-master-class-iota.vercel.app
+        redirect_uri: "https://oauth-master-class-iota.vercel.app/token.html",
+      },
+      "https://oauth-master-class-iota.vercel.app"
     )
-     .then(({ handler }) => handler())
-     .then(async (data) => {
+      .then(({ handler }) => handler())
+      .then(async (data) => {
         const result = await fetchYandexData(data.access_token);
 
         authorize(result);
